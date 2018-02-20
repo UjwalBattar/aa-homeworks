cd4f626d37f133c3da5d006faba0dca44ef3c163
Ujwal Battar <ujwal.battar@gmail.com>
2018-02-19 22:27:23 -0800

    Finish Mancala

--- W2D2/Mancala/lib/board.rb
+++ W2D2/Mancala/lib/board.rb
@@ -1,17 +1,59 @@
+require "byebug"
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14){[]}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    cups[0..5].each do |cup|
+      4.times do
+        cup << :stone
+      end
+    end
+      cups[7..12].each do |cup|
+        4.times do
+          cup << :stone
+        end
+      end
   end
 
   def valid_move?(start_pos)
+    unless [0..13].include?(start_pos) || start_pos.nil?
+      raise "Invalid starting cup"
+    end
   end
 
   def make_move(start_pos, current_player_name)
+    # debugger
+    start_stones = self.cups[start_pos]
+    self.cups[start_pos] = []
+    start_stones
+    idx = start_pos + 1
+    if current_player_name == @name1
+      until start_stones.empty?
+        idx = 0 if idx == 13
+        self.cups[idx] << start_stones.shift
+        idx += 1
+      end
+    else
+      until start_stones.empty?
+        idx = 0 if idx == 14
+        idx = 7 if idx == 6
+        self.cups[idx] << start_stones.shift
+        idx += 1
+      end
+    end
+
+
+    render
+    next_turn(idx)
+    idx - 1
   end
 
   def next_turn(ending_cup_idx)
@@ -27,8 +69,18 @@ class Board
   end
 
   def one_side_empty?
+    self.cups[0..5].all? { |cup| cup.empty? } ||
+    self.cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
+    case self.cups[6].count <=> self.cups[13].count
+    when -1
+      @name2
+    when 0
+      :draw
+    when 1
+      @name1
+    end
   end
 end
